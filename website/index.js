693b9a6e663a9bcc01b4aa13cd9b33d794ce074b
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2022-02-03 02:22:53 +0000

    added a more optimized hyperlink function

--- website/index.js
+++ website/index.js
@@ -55,15 +55,8 @@ var SECURE_SERVERCONNECTED_FLAG_TP = new Proxy({active: false}, {
     }
 });
 
-var wrapURLs = function (text, new_window) {
-    var url_pattern = /(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}\-\x{ffff}]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?/ig;
-    var target = (new_window === true || new_window == null) ? '_blank' : '';
-    
-    return text.replace(url_pattern, function (url) {
-      var protocol_pattern = /^(?:(?:https?|ftp):\/\/)/i;
-      var href = protocol_pattern.test(url) ? url : 'http://' + url;
-      return '<a href="' + href + '" target="' + target + '">' + url + '</a>';
-    });
+function wrap( str ) {
+    return '<a href="' + str + '">' + str + '<\/a>';
 };
 
 document.body.scrollTop = document.documentElement.scrollTop = 0;
@@ -177,7 +170,7 @@ function online(privateKey, publicKey, revocationCertificate) {
         newMsgObj.innerHTML = `
         <span style="font-weight:bold;">${dn}</span>
         <br>
-        <span style="color:silver">${wrapURLs(message)}</span>
+        <span style="color:silver">${message.replace(/\bhttp[^ ]+/ig, wrap)}</span>
         `;
         newMsgObj.classList.add('message');
         messageHolder.appendChild(newMsgObj);
